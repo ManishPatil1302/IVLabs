c50aabb9e7656a18d33f03012ed088182bf0b948
Manish Patil <70889372+ManishPatil1302@users.noreply.github.com>
2020-09-11 22:22:14 +0530

    Update objectrack.py

--- objectrack.py
+++ objectrack.py
@@ -6,7 +6,7 @@ video = cv2.VideoCapture(0)
 while True:
     k,img = video.read()
     cv2.imshow("Tracking",img)
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(30)
     if k == 27:
         break
 rect = cv2.selectROI(img, False)
@@ -25,6 +25,6 @@ while True:
         cv2.rectangle(img, p1, p2, (0,0,255), 2, 2)
 
     cv2.imshow("objectrack", img)
-    k = cv2.waitKey(1) & 0xff
+    k = cv2.waitKey(1)
     if k == 27 : break
 
